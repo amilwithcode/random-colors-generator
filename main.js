f8ba6985ba34f54437388f6f08313039b2c47996
User <mywithcontact@gmail.com>
2025-02-20 16:14:37 +0400

    Random color generating button and css classes added

--- main.js
+++ main.js
@@ -1,26 +1,41 @@
-const colorConEl = document.querySelector(".color-container");
-
-const conEl = document.querySelector(".container");
+const containerEl = document.querySelector(".container");
 
 
 for (let index = 0; index < 30; index++) {
    const colorConEl = document.createElement("div");
 
    colorConEl.classList.add("color-container");
-   conEl.appendChild(colorConEl);
+   containerEl.appendChild(colorConEl);
 }
 
-randomColor();
+
+const colorContainerEls = document.querySelectorAll(".color-container")
+
+
+generateColor()
+
+const button = document.createElement("button");
+button.innerText = "Yenilə";
+button.classList.add('button')
+button.addEventListener("click", generateColor);
+document.body.appendChild(button);
+
+function generateColor() {
+   colorContainerEls.forEach((colorContainerEl) => {
+      const newColorCode = randomColor();
+      colorContainerEl.style.backgroundColor = "#" + newColorCode;
+      colorContainerEl.innerText = "#" + newColorCode;
+   });
+}
 
 function randomColor() {
    const chars = "0123456789abcdef";
    const colorCodeLenght = 6;
    let colorCode = "";
-   for (let index = 0; index < colorCodeLenght.length; index++) {
+   for (let index = 0; index < colorCodeLenght; index++) {
       const randoNum = Math.floor(Math.random() * chars.length);
-      color += chars.substring(randoNum, randoNum + 1);
-      console.log(colorCode, randoNum);
-   };
-
+      colorCode += chars.substring(randoNum, randoNum + 1);
+      console.log(colorCode)
+   }
+   return colorCode
 }
-
---
+++ readme.md
@@ -0,0 +1,4 @@
+<!-- #Random Color Generator  -->
+
+# About 
+This is a web application written in javascript that generates random colors.
